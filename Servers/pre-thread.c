fda267f3cd3f7b7d0c203ae11a0dbce9bb616acc
Pedro R. de Oliveira <de.oliveira.rodriguez@gmail.com>
2016-04-05 00:10:37 -0600

    Intento prethread

--- Servers/pre-thread.c
+++ Servers/pre-thread.c
@@ -5,15 +5,17 @@
 #include <pthread.h>
 
 void *print_message_function( void *ptr );
-void threadChecker(pthread_t *threadListToCheck);
+void threadChecker(int lengthList, pthread_t *threadListToCheck, int *threadStatusList);
+void threadAssigner(int lengthList, pthread_t *threadListToCheck, int *threadStatusList);
 
-int threadsWorking = 0;
-int numberOfThreads;
+
+int iret; //Thread creation code
 
 int main(int argc, char *argv[])
 {
     pthread_t *threadCreator; //Thread to set
-    int iret; //Thread creation code
+    int numberOfThreads;
+
 
     if (argc != 2){
     	printf("ERROR: Input must be like this: ./prethread numberOfThreads\n");
@@ -21,45 +23,23 @@ int main(int argc, char *argv[])
     }
 
     numberOfThreads = atoi(argv[1]);
+    int threadActiveList[numberOfThreads];
     pthread_t *threadList[numberOfThreads];
 
     bzero(threadList, sizeof(numberOfThreads));
+    bzero(threadActiveList, sizeof(numberOfThreads));
 
     //Assign thread on list
     for(int threadCount = 0; threadCount < numberOfThreads; threadCount++){
     	threadList[threadCount] = threadCreator;   	
     }
 
-    //Creator. Check if thread is working
-    for (int threadCount = 0; threadCount < numberOfThreads; threadCount++)
-    {
-     	//Thread occupied
-     	if(pthread_kill(threadList[threadCount], 0) == 0)
-		{
-    		printf("Thread %d occupied. Moving to thread %d\n", threadCount, threadCount+1);
-		}
-		//Thread is free!
-		else 
-		{
-			iret = pthread_create( &threadList[threadCount], NULL, print_message_function, "Thread ready!");
-			if(iret)
-     		{
-     			fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
-     			return 1;
-     		}
-     		printf("Thread no %d created\n", threadCount);
-            threadsWorking++;
-     		break;
-		}
-    }
-
-     /* Wait till threads are complete before main continues. Unless we  */
-     /* wait we run the risk of executing an exit which will terminate   */
-     /* the process and all threads before the threads have completed.   */
+    threadAssigner(numberOfThreads, threadList, threadActiveList);
+    threadAssigner(numberOfThreads, threadList, threadActiveList);
 
     //Stopping dead threads
     while(1){
-        threadChecker(threadList);	
+        threadChecker(numberOfThreads, threadList, threadActiveList);
     }
     
 
@@ -74,21 +54,44 @@ void *print_message_function( void *ptr )
 }
 
 //Checks if there are threads working, in case there is any active ones 
-void threadChecker(pthread_t *threadListToCheck)
+void threadChecker(int lengthList, pthread_t *threadListToCheck, int *threadStatusList)
 {
-    while(threadsWorking != 0){
-        for (int threadCount = 0; threadCount < numberOfThreads; threadCount++)
+    for (int threadCount = 0; threadCount < lengthList; threadCount++)
+    {
+        if(pthread_kill(threadListToCheck[threadCount], 0) == 0)
         {
-            if(pthread_kill(threadListToCheck[threadCount], 0) == 0)
+            printf("Thread no %d is still running. Can't join now\n", threadCount);
+        }
+        else 
+        {
+            pthread_join(threadListToCheck[threadCount], NULL);
+            threadStatusList[threadCount] = 0;
+            printf("Thread no %d is stopped\n", threadCount);
+        }
+    }
+}
+
+void threadAssigner(int lengthList, pthread_t *threadListToCheck, int *threadStatusList)
+{
+    for (int threadCount = 0; threadCount < lengthList; threadCount++)
+    {
+        //Thread occupied
+        if(threadListToCheck[threadCount] == 0)
+        {
+            printf("Thread %d occupied. Moving to thread %d\n", threadCount, threadCount+1);
+        }
+        //Thread is free!
+        else 
+        {
+            iret = pthread_create( &threadListToCheck[threadCount], NULL, print_message_function, "Thread ready!");
+            if(iret)
             {
-                printf("Thread no %d is still running. Can't join now\n", threadCount);
+                fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+                exit(1);
             }
-            else 
-            {
-                pthread_join(threadListToCheck[threadCount], NULL);
-                printf("Thread no %d is stopped\n", threadCount);
-                threadsWorking--;
-            }
+            printf("Thread no %d created\n", threadCount);
+            threadStatusList[threadCount] = 1;
+            break;
         }
     }
 }
